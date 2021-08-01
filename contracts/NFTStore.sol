c778bc5610238aff37d4edb85cd2b09e59cadfac
jpick713 <jpickett713@gmail.com>
2021-08-01 17:27:42 -0400

    added whitelist and blacklist functions

--- contracts/NFTStore.sol
+++ contracts/NFTStore.sol
@@ -12,6 +12,8 @@ import './Ownable.sol';
 contract NFTStore is Ownable{
         mapping (address => bool) public admins;
         mapping (address => bool) public childNFTs;
+        mapping (address => bool) public whiteList;
+        mapping (address => bool) private blackList;
 
         event NFTAdded(address indexed added, uint indexed timeAdded);
 
@@ -36,5 +38,20 @@ contract NFTStore is Ownable{
             childNFTs[_childNFT]= true;
             emit NFTAdded(_childNFT, block.timestamp);
         }
+
+        function addAddressToWhiteList(address _address) public {
+            require (!blackList[_address], "this address has been banned!");
+            require (!whiteList[_address], "this address already on the white list!");
+            whiteList[_address] = true;
+            //possibly emit event
+        }
+
+        function addAddressToBlackList(address _address) public onlyAdmins{
+            require (!blackList[_address], "this address already banned!");
+            blackList[_address] = true;
+            whiteList[_address] = false;
+            //possibly emit event
+        }
+        
         
 }
